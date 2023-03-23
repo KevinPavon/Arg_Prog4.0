0ac24dc9048619ef15cd621589cb5b8205f2eeea
Kevin Pavon <kpavon@binit.dev>
2023-03-23 17:57:31 -0300

    Arg_prog_4.0

---
+++ clase5ejemplo1.js
@@ -0,0 +1,17 @@
+function cambiarColor(){
+    var caja = document.getElementById("caja");
+    var colorEntrada = document.getElementById("color"); //capturamos elemento input
+    var color = colorEntrada.value; //obtenemos el color del elemento input
+    caja.style.backgroundColor = color;
+}
+
+function saludar(){
+    var nombreEntrada = document.getElementById("nombre");
+    var apellidoEntrada = document.getElementById("apellido");
+    var nombre = nombreEntrada.value; 
+    var apellido = apellidoEntrada.value;
+    alert(`¡Hola, ${nombre} ${apellido}!`);
+}
+
+let autos = ["Civic", "Etios", 308];
+console.log(autos[1]);
